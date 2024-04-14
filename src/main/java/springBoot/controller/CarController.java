24008e5328bd08cfb38fe3a8c9e7bd480e2857eb
CoolCat100 <xxdenxxx@mail.ru>
2024-04-15 00:07:25 +0300

    1

--- src/main/java/springBoot/controller/CarController.java
+++ src/main/java/springBoot/controller/CarController.java
@@ -1,17 +1,14 @@
 package springBoot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import springBoot.exception.BadRequestException;
 import springBoot.service.CarService;
 import springBoot.domain.Car;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
@@ -20,8 +17,8 @@ public class CarController {
 
     @GetMapping("/cars")
     public String showCars(@RequestParam(value = "count", required = false, defaultValue = "-1") int limit,
-                           @RequestParam(name = "sortBy", required = false) String sortedBy, Model model) {
-        List<Car> cars = carService.getCars(limit, sortedBy);
+                           @RequestParam(name = "sortBy", required = false) String sortBy, Model model) {
+        List<Car> cars = carService.getCars(limit, sortBy);
         model.addAttribute("list", cars);
         return "cars";
     }
